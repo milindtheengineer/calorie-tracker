ce3d0aca1416d0ff649eb87dcd480860b7d4bc23
Milind Juttiga <milindjuttiga@gmail.com>
2026-03-02 22:06:46 -0800

    just testing

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>vinext smoke test</h1>
+      <p>If you can see this, the app is running.</p>
+    </main>
+  );
+}
